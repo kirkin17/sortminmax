d10abeadd90ea83ef14494a2417b06e40a9e99ad
kirkin17 <kirkin17@users.noreply.github.com>
2020-01-13 03:36:13 +0300

    Добавьте файлы проекта.

---
+++ sortminmax/sortminmax.cpp
@@ -0,0 +1,30 @@
+﻿#include <iostream>
+using namespace std;
+
+
+void main()
+{
+	const int n = 10;
+	int a[n];
+	
+
+	//random
+	for (int i = 0; i < 10; i++)
+	{
+		a[i] = rand() % 20;
+	}
+	//output
+	for (int i = 0; i < 10; i++)
+	{
+		cout << a[i] << "\t";
+	}
+	cout << endl;
+
+
+	//algorithm
+	for (int i = 0; i < 10; i++)
+	{
+		
+	}
+
+}
