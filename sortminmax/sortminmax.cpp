6986f182c35e6ca26b2b1b9e85afce4fdef315c8
kirkin17 <kirkin17@users.noreply.github.com>
2020-01-13 14:56:45 +0300

    final

--- sortminmax/sortminmax.cpp
+++ sortminmax/sortminmax.cpp
@@ -1,37 +1,94 @@
 ﻿#include <iostream>
+#include <ctime>
 using namespace std;
 
+void downtoup(int*);
+void uptodown(int*);
 
+
+const int n = 10;
 void main()
 {
-	const int n = 10;
+	setlocale(LC_ALL,"rus");
 	int a[n];
-	
+	short int menu;
+
 
 	//random
-	for (int i = 0; i < n; i++)
-	{
-		a[i] = rand() % 20;
-	}
-	//output
-	for (int i = 0; i < n; i++)
-	{
-		cout << a[i] << "\t";
-	}
-	cout << endl;
-
-
-	//algorithm downtoup
-	for (int i = 0; i < n; i++)
-	{
-		int min = a[0];
-		for (int j = 1; j < n; j++)
+	while (1) {
+		srand(time(NULL));
+		for (int i = 0; i < n; i++)
 		{
-			if (a[i] < min) min = a[i];
+			a[i] = rand() % 20;
+		}
+		//output
+		for (int i = 0; i < n; i++)
+		{
+			cout << a[i] << "\t";
+		}
+		cout << endl;
+
+		cout << "Сортировка:\n";
+		cout << "1. По возрастанию\n";
+		cout << "2. По убыванию\n";
+		cin >> menu;
+
+		switch (menu)
+		{
+		case 1:
+		{
+			downtoup(a); break;
+		}
+		case 2:
+		{
+			uptodown(a); break;
+		}
 		}
 
+
+		for (int i = 0; i < n; i++)
+		{
+			cout << a[i] << "\t";
+		}
+		system("cls");
 	}
 
+}
+
+	
+//algorithm downtoup 
+void downtoup(int *a_ptr)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int min = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a_ptr[j] < a_ptr[min])
+				min = j;
+		}
+		int tmp = a_ptr[i];
+		a_ptr[i] = a_ptr[min];
+		a_ptr[min] = tmp;
+	}
+}
 
 
+
+
+//algorithm uptodown
+void uptodown(int *a_ptr)
+{
+	for (int i = 0; i < n - 1; i++)
+	{
+		int max = i;
+		for (int j = i + 1; j < n; j++)
+		{
+			if (a_ptr[j] > a_ptr[max])
+				max = j;
+		}
+		int tmp = a_ptr[i];
+		a_ptr[i] = a_ptr[max];
+		a_ptr[max] = tmp;
+	}
 }
